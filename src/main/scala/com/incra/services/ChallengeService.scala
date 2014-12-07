a64207995a4df95340bcb44dcde8a7b869ea2fad
Jeff Risberg <jrisberg@aol.com>
2014-12-06 21:23:24 -0800

    changed dates to this year

--- src/main/scala/com/incra/services/ChallengeService.scala
+++ src/main/scala/com/incra/services/ChallengeService.scala
@@ -14,9 +14,10 @@ object ChallengeService {
   def getEntityList(): List[Challenge] = {
     var challenges = ListBuffer.empty[Challenge]
 
-    challenges += new Challenge(Some(1), "Walk to the Moon", TeamworkType.Team, new Date(213, 5, 6), new Date(213, 5, 20), true)
-    challenges += new Challenge(Some(2), "Fall Hiking", TeamworkType.Team, new Date(213, 3, 4), new Date(213, 3, 16), false)
-    challenges += new Challenge(Some(3), "Holiday Ship-Shape", TeamworkType.Team, new Date(213, 11, 25), new Date(213, 12, 15), false)
+    challenges += new Challenge(Some(1), "Walk to the Moon", TeamworkType.Team, new Date(113, 5, 6), new Date(113, 5, 20), true)
+    challenges += new Challenge(Some(2), "Fall Hiking", TeamworkType.Team, new Date(113, 3, 4), new Date(113, 3, 16), false)
+    challenges += new Challenge(Some(3), "Holiday Ship-Shape", TeamworkType.Team, new Date(113, 11, 25), new Date(113, 12, 15), false)
+    challenges += new Challenge(Some(4), "Work those Pounds Off", TeamworkType.Individual, new Date(113, 12, 25), new Date(114, 01, 15), false)
 
     challenges.toList
   }
