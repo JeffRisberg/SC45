a453c68c6ba15619b70e8117eee089f9127ecace
Jeff Risberg <jrisberg@aol.com>
2014-12-29 14:16:57 -0800

    updated reference version

--- src/main/scala/com/incra/GZipSupportFixed.scala
+++ src/main/scala/com/incra/GZipSupportFixed.scala
@@ -2,75 +2,78 @@ package com.incra
 
 import org.scalatra._
 
-import java.io.PrintWriter
+import java.io.{ByteArrayOutputStream, OutputStream, IOException, PrintWriter}
 import java.util.zip.GZIPOutputStream
-import javax.servlet.ServletOutputStream
+import javax.servlet.{WriteListener, ServletOutputStream}
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpServletResponseWrapper
+import java.nio.charset.Charset
 
 /**
- * Scalatra handler for gzipped responses, with correction to not emit the content-encoding header more than once.
+ * Patched
  *
- * @author Jeffrey Risberg
- * @since 12/23/2014.
+ * Scalatra handler for gzipped responses.
  */
 trait GZipSupportFixed extends Handler {
   self: ScalatraBase =>
 
   abstract override def handle(req: HttpServletRequest, res: HttpServletResponse) {
     withRequestResponse(req, res) {
-      if (isGzip) {
+      println("request check " + isGzip(req))
+      if (isGzip(req)) {
         val gzos = new GZIPOutputStream(res.getOutputStream)
         val w = new PrintWriter(gzos)
-        val gzsos = new ServletOutputStream {
-          def write(b: Int) {
-            gzos.write(b)
-          }
-        }
-
-        val response = new HttpServletResponseWrapper(res) {
-          override def getOutputStream: ServletOutputStream = gzsos
-
-          override def getWriter: PrintWriter = w
-
-          override def setContentLength(i: Int) = {} // ignoring content length as it won't be the same when gzipped
-        }
-
-        /** BEGIN PATCH */
-
-        ScalatraBase onCompleted { _ =>
-          val currentValue = response.getHeader("Content-Encoding")
-          if (currentValue == null || currentValue.indexOf("gzip") == -1) {
-            println("adding gzip header")
-            response.addHeader("Content-Encoding", "gzip")
-          }
-          else {
-            println("skipping adding gzip header")
-          }
-        }
-
-        /** END PATCH */
+        val r = response
 
         ScalatraBase onRenderedCompleted { _ =>
           w.flush()
           w.close()
         }
 
-        withRequestResponse(req, response) {
-          super.handle(req, response)
+        val gzsos = new GZipServletOutputStream(gzos, r.getOutputStream)
+        val wrapped = new WrappedGZipResponse(r, gzsos, w)
+
+        ScalatraBase.onCompleted { _ => {
+          println("response check " + isGzip(res))
+          wrapped.addHeader("Content-Encoding", "gzip")
         }
+        }
+
+        super.handle(req, wrapped)
       } else {
         super.handle(req, res)
       }
     }
   }
 
+  private class GZipServletOutputStream(gzos: GZIPOutputStream, orig: ServletOutputStream) extends ServletOutputStream {
+    override def write(b: Int): Unit = gzos.write(b)
+
+    override def setWriteListener(writeListener: WriteListener): Unit = orig.setWriteListener(writeListener)
+
+    override def isReady: Boolean = orig.isReady()
+  }
+
+  private class WrappedGZipResponse(res: HttpServletResponse, gzsos: ServletOutputStream, w: PrintWriter) extends HttpServletResponseWrapper(res) {
+    override def getOutputStream: ServletOutputStream = gzsos
+
+    override def getWriter: PrintWriter = w
+
+    override def setContentLength(i: Int) = {} // ignoring content length as it won't be the same when gzipped
+  }
+
   /**
    * Returns true if Accept-Encoding contains gzip.
    */
-  private[this] def isGzip(implicit request: HttpServletRequest): Boolean = {
-    Option(request.getHeader("Accept-Encoding")).getOrElse("")
-      .toUpperCase.contains("GZIP")
+  private[this] def isGzip(request: HttpServletRequest): Boolean = {
+    Option(request.getHeader("Accept-Encoding")).getOrElse("").toUpperCase.contains("GZIP")
+  }
+
+  /**
+   * Returns true if Accept-Encoding contains gzip.
+   */
+  private[this] def isGzip(response: HttpServletResponse): Boolean = {
+    Option(response.getHeader("Content-Encoding")).getOrElse("").toUpperCase.contains("GZIP")
   }
 }
